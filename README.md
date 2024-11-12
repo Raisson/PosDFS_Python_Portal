ddd9c4095b87e60c463837010ab2fd38ad7337e6
ProjetosTIP1 <raissoncharles@gmail.com>
2024-11-12 13:13:01 -0300

    pesquisa e paginação portugues, readme

--- README.md
+++ README.md
@@ -20,10 +20,38 @@ python manage.py runserver
 
 ## Passo : Rotas api
 
+Requisições GET
+
+Endpoints:
 localhost:8000/api/projetos/
 localhost:8000/api/projetos/<ID>
 localhost:8000/api/atividades/
 localhost:8000/api/atividades/<ID>
 localhost:8000/api/projetos-atividades/
+
+
+Requisições POST
+
+Endpoint: localhost:8000/api/projetos/
+Body:
+  {
+    "nome": "Projeito 15",
+    "descricao": "Descrição Projeito 15",
+    "percentual_concluido": 33,
+    "data_inicio": "2025-11-01",
+    "data_fim": "2025-11-25"
+  }
+
+Endpoint: localhost:8000/api/atividades/
+Body:
+  {
+    "nome": "Atividade 15",
+    "projeto": 1,
+    "user": 1,
+    "status_atividade": "FINALIZADO",
+    "prioridade": "ALTA",
+    "descricao": "Descrição Atividade 15"
+  }
+
 
 
--- app/serializers.py
+++ app/serializers.py
@@ -5,16 +5,16 @@ class ProjetosSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Projetos
-        fields = ['id', 'nome', 'descricao','percentual_concluido']
+        fields = ['id', 'nome', 'descricao','percentual_concluido','data_inicio', 'data_fim']
 
 class AtividadesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Atividades
-        fields = ['id', 'nome', 'projeto', 'user', 'status_atividade', 'descricao']
+        fields = ['id', 'nome', 'projeto', 'user', 'status_atividade', 'prioridade','descricao']
 
 class ProjetosAtividadesSerializer(serializers.ModelSerializer):
     atividades = AtividadesSerializer(many=True, read_only=True)
 
     class Meta:
         model = Projetos
-        fields = ['id', 'nome', 'descricao','percentual_concluido', 'atividades']
+        fields = ['id', 'nome', 'descricao','percentual_concluido', 'data_inicio', 'data_fim','atividades']
